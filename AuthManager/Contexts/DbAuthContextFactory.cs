4b04e26c404d3d1e00da1ce716d23b418104b67f
Jannik Ellendt <jannik.ellendt@web.de>
2026-03-07 23:20:36 +0100

    feat: add database migration for removed fields

---
+++ AuthManager/Contexts/DbAuthContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AuthManager.Contexts
+{
+    //needed for generating ef core migrations
+    public class DbAuthContextFactory : IDesignTimeDbContextFactory<DbAuthContext>
+    {
+        public DbAuthContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<DbAuthContext> optionsBuilder = new();
+
+            optionsBuilder.UseSqlServer("Server=...");
+
+            return new DbAuthContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ AuthManager/Migrations/20260307221531_edit-user-auth.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class edituserauth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Salt",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
